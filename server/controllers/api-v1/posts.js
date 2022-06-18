1642abbcb266e03f850953e3308b81a4ed70778c
philjang <pjang7390@gmail.com>
2022-06-17 23:50:43 -0700

    add posts controller boilerplate

--- server/controllers/api-v1/posts.js
+++ server/controllers/api-v1/posts.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// POST /api-v1/posts
+router.post('/', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: POST /posts');
+});
+
+// PUT /api-v1/posts/:id
+router.put('/:id', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: PUT /posts/:id');
+});
+
+// DELETE /api-v1/posts/:id
+router.delete('/:id', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: DELETE /posts/:id');
+});
+
 module.exports = router;
