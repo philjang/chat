121c47f9dd3a2e2ba2129f0eae531ca4c4c46482
philjang <pjang7390@gmail.com>
2022-06-17 23:37:36 -0700

    add users controller boilerplate

--- server/controllers/api-v1/users.js
+++ server/controllers/api-v1/users.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 router.get('/', async (req,res) => {
     try {
-
+        res.send('/users');
     } catch (error) {
-        console.log(err)
-        res.status(503).json({ msg : 'error'})
-    }
-})
+        console.log(err);
+        res.status(503).json({ msg : 'error'});
+    };
+});
+
+// POST /api-v1/users/register
+router.post('/register', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: POST /users/register');
+});
+
+// POST /api-v1/users/login
+router.post('/login', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: POST /users/login');
+});
+
+// PUT /api-v1/users/:id
+router.put('/:id', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: PUT /users/:id');
+});
+
+// DELETE /api-v1/users/:id
+router.delete('/:id', async (req, res) => {
+    res.send('NOT YET IMPLEMENTED: DELETE /users/:id');
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 
 app.get('/', (req,res) => {
-    res.json({ msg: 'hello'})
-})
+    res.json({ msg: 'hello'});
+});
 
 // controllers
-app.use('/api-v1/users', require('./controllers/api-v1/users'))
-app.use('/api-v1/rooms', require('./controllers/api-v1/rooms'))
-app.use('/api-v1/posts', require('./controllers/api-v1/posts'))
+app.use('/api-v1/users', require('./controllers/api-v1/users'));
+app.use('/api-v1/rooms', require('./controllers/api-v1/rooms'));
+app.use('/api-v1/posts', require('./controllers/api-v1/posts'));
 
 
 app.listen(PORT, () => {
-    console.log(`running server on port ${PORT}`)
-})
+    console.log(`running server on port ${PORT}`);
+});
